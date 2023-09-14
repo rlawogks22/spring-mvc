34df3d5ecd617ca2644216abacd6f75ccc1e488a
rlawogks22 <rlawogks1234@gmail.com>
2023-09-14 14:53:09 +0900

    mvc2

--- src/main/java/com/example/springmvc/html/HtmlController.java
+++ src/main/java/com/example/springmvc/html/HtmlController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
-    private static long visitCount  = 0;
+    private static long visitCount  = 0;//카운트
     @GetMapping("/static-hello")
     public String hello(){
         return "hello html";
     }
 
-    @GetMapping("/html/redirect")
+    @GetMapping("/html/redirect")//정적
     public String htmlStatic(){
         return "redirect:/hello.html";
     }
 
-    @GetMapping("/html/templates")
+    @GetMapping("/html/templates")//동적
     public String htmlTemplates(){
         return "hello";
     }
 
-    @GetMapping("/html/dynamic")
+    @GetMapping("/html/dynamic")//동적
     public String htmlDynamic(Model model){
         visitCount++;
         model.addAttribute("visits",visitCount);
