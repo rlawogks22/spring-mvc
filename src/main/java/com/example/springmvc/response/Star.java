cc0335f56abb86a15746bfb20b91cc2cb86ea800
rlawogks22 <rlawogks1234@gmail.com>
2023-09-14 15:32:38 +0900

    데이터를 Client에 반환 하는 방법

---
+++ src/main/java/com/example/springmvc/response/Star.java
@@ -0,0 +1,15 @@
+package com.example.springmvc.response;
+import lombok.*;
+
+@Getter
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Star() {}
+}
